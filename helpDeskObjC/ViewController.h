a025f3addfe2fbe0b414fbd1e54ee7e0df8c95e8
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-09-30 10:13:22 +0300

    Initial Commit

---
+++ helpDeskObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helpDeskObjC
+//
+//  Created by Alexey Chulochnikov on 30.09.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ helpDeskObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helpDeskObjC
+//
+//  Created by Alexey Chulochnikov on 30.09.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
