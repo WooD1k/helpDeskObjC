60cc4cc04190f5efe5b9932d5dbcce953f1943be
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-10-02 18:45:22 +0300

    - collection view added to the project

---
+++ helpDeskObjC/issuesListViewController.h
@@ -0,0 +1,19 @@
+//
+//  issuesListViewController.h
+//  helpDeskObjC
+//
+//  Created by Alexey Chulochnikov on 02.10.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface issuesListViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@property (nonatomic) NSUInteger issuesQty;
+@property (nonatomic) NSArray *issuesArray;
+
+- (IBAction)closeResentIssues:(UIButton *)sender;
+
+@end
---
+++ helpDeskObjC/issuesListViewController.m
@@ -0,0 +1,75 @@
+//
+//  issuesListViewController.m
+//  helpDeskObjC
+//
+//  Created by Alexey Chulochnikov on 02.10.14.
+//  Copyright (c) 2014 Alexey Chulochnikov. All rights reserved.
+//
+
+#import "issuesListViewController.h"
+#import <Parse/Parse.h>
+
+@interface issuesListViewController ()
+
+@end
+
+@implementation issuesListViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+	PFQuery* issuesQuery = [PFQuery queryWithClassName:@"issues"];
+	
+	[issuesQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+		if (error) {
+			NSLog(@"error: %@", error);
+		} else {
+			_issuesQty = [objects count];
+			_issuesArray = objects;
+			
+			[self.collectionView reloadData];
+		}
+	}];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UICollectionViewDelegate methods
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+	return _issuesQty;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *cellName = @"issueCell";
+	
+	UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellName forIndexPath:indexPath];
+	
+	PFImageView *imageView = [[PFImageView alloc] init];
+	imageView.file = [_issuesArray[indexPath.row] objectForKey:@"image"];
+	[imageView loadInBackground];
+	
+	cell.backgroundView = imageView;
+	
+	return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)closeResentIssues:(UIButton *)sender {
+	[self dismissViewControllerAnimated:true completion:nil];
+}
+@end
